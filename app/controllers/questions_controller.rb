cb5a4fa09a08a2ee91064c97b6c46309d11917e9
Ivan Rodionov <uhf_job@mail.ru>
2023-01-31 18:48:04 +0300

    Remove raise from .create and .destroy in QuestionsController

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,8 @@
 
 class QuestionsController < ApplicationController
   before_action :find_question, only: %i[show]
-  before_action :find_test, only: %i[new]
+  before_action :find_test, only: %i[new create]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :rescue_with_question_invalid
 
   def index
     render inline: "<%= Test.find(params[:test_id]).questions.pluck(:body) %>"
@@ -15,14 +14,14 @@ class QuestionsController < ApplicationController
   def new; end
 
   def create
-    question = Question.create!(params.permit(:body, :test_id))
-    render plain: question.id.nil? ? question.errors.inspect : "Вопрос успешно создан"
+    question = @test.questions.create(question_params)
+    render plain: question.valid? ? "Вопрос успешно создан" : question.errors.inspect
   end
 
   def destroy
     question = Question.find(params[:id])
-    question.destroy!
-    render plain: question.persisted? ? question.errors.inspect : "Вопрос успешно удален"
+    question.destroy
+    render plain: question.persisted? ? "Ошибка: Неудачная попытка удаления" : "Вопрос успешно удален"
   end
 
   private
@@ -32,14 +31,14 @@ class QuestionsController < ApplicationController
   end
 
   def find_question
-    @question = find_test.questions.find(params[:id])
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
   end
 
   def rescue_with_question_not_found
     render plain: "Ошибка: Вопрос или вопросы не найдены"
-  end
-
-  def rescue_with_question_invalid(exeption)
-    render plain: "Ошибка: Вопрос должен содержать не менее 1 символа" if exeption.record[:body].blank?
   end
 end
