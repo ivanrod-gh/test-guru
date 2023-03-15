075092169601f4e69c489238e70d33e48e7e953d
Ivan Rodionov <uhf_job@mail.ru>
2023-03-15 14:25:15 +0300

    Rewrite password.confirmation.js

--- app/javascript/packs/utilities/password.confirmation.js
+++ app/javascript/packs/utilities/password.confirmation.js
@@ -1,36 +1,37 @@
 document.addEventListener('turbolinks:load', function() {
-  var control = document.querySelector('#user_password_confirmation')
-  if (control) { 
-    control.addEventListener('input', checkConfirmationCoincidence)
-    var control = document.querySelector('#user_password')
-    if (control) { control.addEventListener('input', checkConfirmationCoincidence) }
+  var password = document.querySelector('#user_password')
+  var confirmation = document.querySelector('#user_password_confirmation')
+  if (password && confirmation) { 
+    password.addEventListener('input', checkPasswordConfirmationCoincidence)
+    confirmation.addEventListener('input', checkPasswordConfirmationCoincidence)
   }
 })
 
-function checkConfirmationCoincidence() {
-  var confirmation = document.querySelector('#user_password_confirmation')
+function checkPasswordConfirmationCoincidence() {
   var password = document.querySelector('#user_password')
+  var confirmation = document.querySelector('#user_password_confirmation')
   var alertIcon = document.querySelector('.octicon-alert')
-  var tooltipPassed = document.querySelector('#tooltip-password-passed').textContent
-  var tooltipFailed = document.querySelector('#tooltip-password-failed').textContent
+  var tooltipPassedText = document.querySelector('#tooltip-password-passed').textContent
+  var tooltipFailedText = document.querySelector('#tooltip-password-failed').textContent
 
-  if (password.value && confirmation.value) {
-    if (password.value == confirmation.value) {
-      alertIcon.classList.remove('hide')
-      alertIcon.classList.remove('red')
-      alertIcon.classList.add('green')
-      confirmation.setAttribute('title', tooltipPassed)
-      confirmation.style.borderColor = 'green'
-    } else {
-      alertIcon.classList.remove('hide')
-      alertIcon.classList.remove('green')
-      alertIcon.classList.add('red')
-      confirmation.setAttribute('title', tooltipFailed)
-      confirmation.style.borderColor = 'red'
-    }
-  } else if (!password.value || !confirmation.value) {
+  if (!password.value || !confirmation.value) {
     alertIcon.classList.add('hide')
     confirmation.setAttribute('title', '')
     confirmation.style.borderColor = ''
+    return
+  }
+
+  if (password.value == confirmation.value) {
+    alertIcon.classList.remove('hide')
+    alertIcon.classList.remove('red')
+    alertIcon.classList.add('green')
+    confirmation.setAttribute('title', tooltipPassedText)
+    confirmation.style.borderColor = 'green'
+  } else {
+    alertIcon.classList.remove('hide')
+    alertIcon.classList.remove('green')
+    alertIcon.classList.add('red')
+    confirmation.setAttribute('title', tooltipFailedText)
+    confirmation.style.borderColor = 'red'
   }
 }
