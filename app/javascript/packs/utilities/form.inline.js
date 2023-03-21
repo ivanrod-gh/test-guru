a5661c71d798e703030ddbdf7523a1344ed77a9b
Ivan Rodionov <uhf_job@mail.ru>
2023-03-21 16:24:50 +0300

    Remove unused jQuery code

--- app/javascript/packs/utilities/form.inline.js
+++ app/javascript/packs/utilities/form.inline.js
@@ -9,11 +9,6 @@ document.addEventListener('turbolinks:load', function() {
     }
   }
 
-  // $('.form-inline-link').each(function() {
-  //   $(this).on('click', formInlineLinkHandler)
-  //   $(this).removeAttr('data-method')
-  // })
-
   var error = document.querySelector('.instance-errors')
   if (error) {
     var instanceId = error.dataset.instanceId
@@ -31,23 +26,14 @@ function formInlineHandler(testId) {
   var link = document.querySelector('.form-inline-link[data-test-id="' + testId + '"]')
   var testTitle = document.querySelector('.test-title[data-test-id="' + testId + '"]')
   var row = document.querySelector('.row[data-test-id="' + testId + '"]')
-  // var $link = $('.form-inline-link[data-test-id="' + testId + '"]')
-  // var $testTitle = $('.test-title[data-test-id="' + testId + '"]')
-  // var $row = $('.row[data-test-id="' + testId + '"]')
-
-  // $testTitle.toggle()
-  // $row.toggle()
 
   if (row.classList.contains('hide')) {
-  // if ($row.is(':visible')) {
     testTitle.classList.add('hide')
     row.classList.remove('hide')
     link.textContent = document.querySelector('.admin-author-actions-cancel').textContent
-    // $link.textContent = 'Cancel'
   } else {
     testTitle.classList.remove('hide')
     row.classList.add('hide')
     link.textContent = document.querySelector('.admin-author-actions-edit').textContent
-    // $link.textContent = 'Edit'
   }
 }
